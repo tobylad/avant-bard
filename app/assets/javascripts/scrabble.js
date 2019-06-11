bbd62af537d006721e8d776495088119aae883a9
majesty86 <tladislas@gmail.com>
2019-06-10 21:25:54 -0400

    Remove let definitions.

--- app/assets/javascripts/scrabble.js
+++ app/assets/javascripts/scrabble.js
@@ -3,30 +3,28 @@ $(document).ready(function() {
     generateListTitles();
     combineWordsByLength();
   }
-
-
 });
 
-let createListTitle = function(lengthString) {
+var createListTitle = function(lengthString) {
   return "<p class='word-length'>" + lengthString + "-letter words:</p>";
 }
 
-let generateListTitles = function() {
+var generateListTitles = function() {
   $(".word-list").each(function() {
-    let list = $(this);
-    let wordLength = list.find("li").first().text().length.toString();
+    var list = $(this);
+    var wordLength = list.find("li").first().text().length.toString();
     list.parent().prepend(createListTitle(wordLength));
   });
 
 }
  
-let combineWordsByLength = function() {
-  let listWordLengths = [];
+var combineWordsByLength = function() {
+  var listWordLengths = [];
   $(".word-length").each(function() {
     listWordLengths.push(($(this).text().replace("-letter words:", "")));
   });
 
-  let maxLength = Math.max.apply(null, listWordLengths);
+  var maxLength = Math.max.apply(null, listWordLengths);
 
   console.log(maxLength);
 
